0793cc5542e84936dfdbd1199ba397cd970d4588
Vkutti <70446320+Vkutti@users.noreply.github.com>
2024-11-19 16:09:11 -0800

    Add files via upload

---
+++ Project FAT/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/business1")
+def business1():
+    return render_template("business1.html")
+    
+if __name__ == "__main__":
+    app.run()
