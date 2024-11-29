c8e82ed502f580e15ffb4cbb047790509f153d68
Venkat Kutti <venkat.k.kutti@gmail.com>
2024-11-28 17:20:53 -0800

    the boxes automatically populate with the correct names of businesses IT WORKS :))

--- projectFAT/main.py
+++ projectFAT/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, redirect, request
 import sqlite3
 from cs50 import SQL
+import json
 
 
 # db = SQL("sqlite:////absolute/path/to/FAT.db")
@@ -24,6 +25,7 @@ def business1():
     print(data)
 
     b_name = db.execute(f"SELECT `BusinessName` FROM fat WHERE SPECS = '{str(data.upper())}'")
+
     vals = int(len(b_name))
 
     print(b_name)
@@ -33,8 +35,16 @@ def business1():
         businesses.append(b_name[val]['BusinessName'])
         # selected_business = b_name[0]['BusinessName'] 
         print(businesses) 
+
+        if businesses == []:
+            businesses.append("No business in {} category were found")
         
-    return render_template("searchResults.html", sb=businesses[0], category=data.lower(), values=vals)
+    # businessnamejson = json.dumps(businesses)
+    # print(businessnamejson)
+
+
+
+    return render_template("searchResults.html", sb=businesses, category=data.lower(), values=vals)
     
 if __name__ == "__main__":
     app.run()
